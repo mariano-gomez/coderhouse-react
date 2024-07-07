b45ed573954d9f1c6aca116931dbd71d966d1ecc
DESKTOP-AK750H5\Mariano <marianogomez@gmail.com>
2024-07-07 17:43:33 -0300

    Adding CartContext to the App.jsx, refactoring the imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
-import NavBar from "./components/NavBar/NavBar.jsx";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { CartContextProvider } from './context/CartContext.jsx';
+import NavBar from "./components/NavBar/NavBar.jsx";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer.jsx";
 import PageNotFound from "./components/PageNotFound/PageNotFound.jsx";
 
@@ -10,15 +12,16 @@ function App() {
 
     return (
         <ChakraProvider>
-            <BrowserRouter>
-                <NavBar/>
-                <Routes>
-                    <Route path='/' element={<ItemListContainer title={'Tienda (general)'} />} />
-                    <Route path='/category/:categoryId' element={<ItemListContainer title={'Tienda'} />} />
-                    <Route path='/product/:productId' element={<ItemDetailContainer title={'Detalles del producto'}  />} />
-                    <Route path='*' element={<PageNotFound />} />
-                </Routes>
-            </BrowserRouter>
+            <CartContextProvider>
+                <BrowserRouter>
+                    <NavBar/>
+                    <Routes>
+                        <Route path='/' element={<ItemListContainer title={'Tienda (general)'} />} />
+                        <Route path='/category/:categoryId' element={<ItemListContainer title={'Tienda'} />} />
+                        <Route path='/product/:productId' element={<ItemDetailContainer title={'Detalles del producto'}  />} />
+                    </Routes>
+                </BrowserRouter>
+            </CartContextProvider>
         </ChakraProvider>
     )
 }
