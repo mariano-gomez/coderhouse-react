20992f82f976b79c600a4c5a5ff370ffd9141d66
DESKTOP-AK750H5\Mariano <marianogomez@gmail.com>
2024-07-07 16:23:36 -0300

    Updating CartWidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,47 +1,20 @@
 import React from 'react';
-import {Box, Button} from "@chakra-ui/react";
-
-import {FaShoppingCart } from 'react-icons/fa';
+import { Button, Link as ChakraLink } from "@chakra-ui/react";
+import { FaShoppingCart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const CartWidget = () => {
-
-    const [isOpen, setIsOpen] = React.useState(false);
-
-    const handleOpen = () => {
-        setIsOpen(true);
-    };
-
-    const handleClose = () => {
-        setIsOpen(false);
-    };
 
     return (
         <>
             <Button
-                onClick={handleOpen}
                 variant="ghost"
                 leftIcon={<FaShoppingCart />}
             >
-                Carrito
+                <ChakraLink as={Link} to='/cart' >
+                    Carrito
+                </ChakraLink>
             </Button>
-
-            {/* cart Popup */}
-            {isOpen && (
-                <Box
-                    position="absolute"
-                    top="40px"
-                    right="0"
-                    width="300px"
-                    bg="white"
-                    padding={4}
-                    boxShadow="lg"
-                    zIndex={11}
-                >
-                    <h3>Carrito de compras</h3>
-
-                    <Button onClick={handleClose}>Cerrar</Button>
-                </Box>
-            )}
         </>
     );
 }
